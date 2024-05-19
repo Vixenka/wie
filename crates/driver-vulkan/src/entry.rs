d0cd304e6572cdacbd20382a61dfbcc65e8fdd5a
Vixen <infvix@gmail.com>
2024-05-19 20:59:22 +0200

    Add more entries

--- crates/driver-vulkan/src/entry.rs
+++ crates/driver-vulkan/src/entry.rs
@@ -16,3 +16,32 @@ extern "C" fn vk_icdGetInstanceProcAddr(
     std::thread::sleep(std::time::Duration::from_millis(1000));
     unimplemented!();
 }
+
+#[no_mangle]
+extern "C" fn vk_icdGetPhysicalDeviceProcAddr(
+    _instance: vk::Instance,
+    _p_name: *const c_char,
+) -> vk::PFN_vkVoidFunction {
+    println!("bao2");
+    std::fs::write(
+        "C:\\Users\\Vixen\\Desktop\\wie-logs\\vk_icdGetPhysicalDeviceProcAddr.txt",
+        "whoa",
+    )
+    .unwrap();
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    unimplemented!();
+}
+
+#[no_mangle]
+extern "C" fn vk_icdNegotiateLoaderICDInterfaceVersion(
+    _p_supported_version: *mut u32,
+) -> vk::Result {
+    println!("bao3");
+    std::fs::write(
+        "C:\\Users\\Vixen\\Desktop\\wie-logs\\vk_icdNegotiateLoaderICDInterfaceVersion.txt",
+        "whoa",
+    )
+    .unwrap();
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    unimplemented!();
+}
