12075f35f0c293df26ffe0babb5e7fb34443b4d0
Vixen <infvix@gmail.com>
2024-05-20 16:17:24 +0200

    Fix building on Windows

--- crates/driver-vulkan/install_windows.bat
+++ crates/driver-vulkan/install_windows.bat
@@ -6,7 +6,7 @@ echo "Building wie-driver-vulkan..."
 cargo build --release || echo "Failed!" && exit 1
 
 echo "Create directories..."
-mkdir "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
+mkdir "C:\Program Files\Vixen\wie\Vulkan\"
 echo "Copying ICD file..."
 copy "%batdir%vk_wie_icd.json" "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
 echo "Copying driver file..."
