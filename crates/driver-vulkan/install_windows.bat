e5b20461fc9c725603e1736ddcf6e3f7366bea48
Vixen <infvix@gmail.com>
2024-05-20 09:19:41 +0200

    Add driver.def

--- crates/driver-vulkan/install_windows.bat
+++ crates/driver-vulkan/install_windows.bat
@@ -1,16 +1,18 @@
+@echo off
+
 set batdir=%~dp0
 
 echo "Building wie-driver-vulkan..."
-cargo build --release
+cargo build --release || echo "Failed!" && exit 1
 
 echo "Create directories..."
-mkdir "C:\Program Files\Vixen\wie\Vulkan\"
+mkdir "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
 echo "Copying ICD file..."
-copy "%batdir%vk_wie_icd.json" "C:\Program Files\Vixen\wie\Vulkan\"
+copy "%batdir%vk_wie_icd.json" "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
 echo "Copying driver file..."
-copy "%batdir%..\..\target\release\wie_driver_vulkan.dll" "C:\Program Files\Vixen\wie\Vulkan\"
+copy "%batdir%..\..\target\release\wie_driver_vulkan.dll" "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
 
 echo "Update registry..."
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\Drivers" /v "C:\Program Files\Vixen\wie\Vulkan\vk_wie_icd.json" /t REG_DWORD /d 0 /f
+regedit.exe /S "%batdir%driver.reg" || echo "Failed!" && exit 1
 
 echo "Vulkan driver successfully installed."
