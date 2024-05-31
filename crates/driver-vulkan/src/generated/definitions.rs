3bd399dd399bea3d5bda533cdfaa4cba1268115d
Vixen <infvix@gmail.com>
2024-05-31 19:55:35 +0200

    Add warn with parameters

--- crates/driver-vulkan/src/generated/definitions.rs
+++ crates/driver-vulkan/src/generated/definitions.rs
@@ -1025,6 +1025,10 @@ unsafe extern "stdcall" fn vkEnumerateInstanceExtensionProperties(
     p_property_count: *mut u32,
     p_properties: *mut vk::ExtensionProperties,
 ) -> vk::Result {
+    warn!(
+        "vkEnumerateInstanceExtensionProperties {:?} {:?} {:?}",
+        p_layer_name, p_property_count, p_properties
+    );
     //let mut packet = wie_transport_guest::new_packet(5);
     //packet.write_null_str(p_layer_name);
     //packet.write(*p_property_count);
