e0a813fe972a3f90704cd6d6efb8f030d61e49b5
Vixen <infvix@gmail.com>
2024-05-31 20:50:16 +0200

    Edit install_windows.bat

--- crates/driver-vulkan/install_windows.bat
+++ crates/driver-vulkan/install_windows.bat
@@ -10,7 +10,7 @@ mkdir "C:\Program Files\Vixen\wie\Vulkan\"
 echo "Copying ICD file..."
 copy "%batdir%vk_wie_icd.json" "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
 echo "Copying driver file..."
-copy "%batdir%..\..\target\release\wie_driver_vulkan.dll" "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
+copy "%batdir%..\..\target\debug\wie_driver_vulkan.dll" "C:\Program Files\Vixen\wie\Vulkan\" || echo "Failed!" && exit 1
 
 echo "Update registry..."
 regedit.exe /S "%batdir%driver.reg" || echo "Failed!" && exit 1
